d8a6825da7a930babd71681c5e5fee390100d738
Xiangfeng Zhu <xzhu0027@gmail.com>
2023-04-10 18:17:19 +0000

    add load balancer element

---
+++ elements/local/l4loadbalancer.cc
@@ -0,0 +1,41 @@
+#include <click/config.h>
+#include "l4loadbalancer.hh"
+CLICK_DECLS
+
+#define SERVER_IP 0xC0A80001
+
+Packet *
+L4LoadBalancer::simple_action(Packet *p) {
+    click_chatter("Received a packet of size %d !", p->length());
+
+    // Extract 5 tuple
+    const click_ip *iph = p->ip_header();
+    const click_tcp *tcph = p->tcp_header();
+    uint32_t src_ip = iph->ip_src.s_addr;
+    uint32_t dst_ip = iph->ip_dst.s_addr;
+    uint16_t src_port = tcph->th_sport;
+    uint16_t dst_port = tcph->th_dport;
+    uint8_t protocol = iph->ip_p;
+    FlowTuple f = {src_ip, dst_ip, src_port, dst_port, protocol};
+
+    IPAddress server_ip = IPAddress(SERVER_IP);
+    if (connection_table.find(f) == connection_table.end()) {
+        // new flow
+        connection_table[f] = server_ip;
+    } else {
+        // existing flow
+        server_ip = connection_table[f];
+    }   
+
+    WritablePacket* q =p->uniqueify();
+    p = q;
+
+    q->ip_header()->ip_dst = server_ip;
+    p->set_dst_ip_anno(server_ip);
+
+    click_chatter("Destination IP is %u\n", p->ip_header()->ip_dst.s_addr);
+    return p;
+}
+
+CLICK_ENDDECLS
+EXPORT_ELEMENT(L4LoadBalancer)
---
+++ elements/local/l4loadbalancer.hh
@@ -0,0 +1,55 @@
+#ifndef CLICK_L4LOADBALANCER_HH
+#define CLICK_L4LOADBALANCER_HH
+#include <unordered_map>
+#include <click/batchelement.hh>
+CLICK_DECLS
+
+
+struct FlowTuple {
+    uint32_t src_ip;    // Source IP address
+    uint32_t dst_ip;    // Destination IP address
+    uint16_t src_port;  // Source port number
+    uint16_t dst_port;  // Destination port number
+    uint8_t protocol;  // Protocol number 
+
+    bool operator==(const FlowTuple& other) const {
+        return (src_ip == other.src_ip && dst_ip == other.dst_ip &&
+                src_port == other.src_port && dst_port == other.dst_port &&
+                protocol == other.protocol);
+    }
+};
+
+struct FlowTupleHash {
+    std::size_t operator()(const FlowTuple& ft) const {
+        std::size_t hash = 17;
+        hash = hash * 31 + std::hash<unsigned int>()(ft.src_ip);
+        hash = hash * 31 + std::hash<unsigned int>()(ft.dst_ip);
+        hash = hash * 31 + std::hash<unsigned short>()(ft.src_port);
+        hash = hash * 31 + std::hash<unsigned short>()(ft.dst_port);
+        hash = hash * 31 + std::hash<unsigned char>()(ft.protocol);
+        return hash;
+    }
+};
+
+struct FlowTupleEqual {
+    bool operator()(const FlowTuple& ft1, const FlowTuple& ft2) const {
+        return (ft1 == ft2);
+    }
+};
+
+
+class L4LoadBalancer : public SimpleElement<L4LoadBalancer> { public:
+
+    L4LoadBalancer() CLICK_COLD {};
+
+    const char *class_name() const              { return "L4LoadBalancer"; }
+    const char *port_count() const              { return PORTS_1_1; }
+
+    Packet *simple_action(Packet *);
+private:
+    // Connection table: Flow 5 tuple -> server ip
+    std::unordered_map<FlowTuple, IPAddress, FlowTupleHash, FlowTupleEqual> connection_table;
+};
+
+CLICK_ENDDECLS
+#endif
